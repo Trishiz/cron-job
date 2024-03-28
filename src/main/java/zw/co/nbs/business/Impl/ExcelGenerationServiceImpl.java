4527687f3d7cc089437440071602a6c461567c44
Trishiz <trish.majoni@nbs.co.com>
2024-03-28 17:18:19 +0200

    smtp

--- src/main/java/zw/co/nbs/business/Impl/ExcelGenerationServiceImpl.java
+++ src/main/java/zw/co/nbs/business/Impl/ExcelGenerationServiceImpl.java
@@ -5,10 +5,16 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.context.ApplicationContext;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import zw.co.nbs.business.api.ExcelGenerationService;
 import zw.co.nbs.response.dto.TransactionReportDto;
+import org.springframework.mail.javamail.JavaMailSender;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -18,6 +24,10 @@ import java.util.List;
 @Slf4j
 @Service
 public class ExcelGenerationServiceImpl implements ExcelGenerationService {
+    private final JavaMailSender mailSender;
+    public ExcelGenerationServiceImpl(final ApplicationContext context){
+        this.mailSender=context.getBean(JavaMailSender.class);
+    }
     public void generateExcelAndSendEmail(List<TransactionReportDto> transactionReportDtoList) {
         List<TransactionReportDto> items = transactionReportDtoList;
 
@@ -40,8 +50,6 @@ public class ExcelGenerationServiceImpl implements ExcelGenerationService {
             dataRow.createCell(3).setCellValue(transactionReportDto.getResponseCodeStatus());
             dataRow.createCell(4).setCellValue(transactionReportDto.getNumberOfTransactions());
         }
-
-
         String path = System.getProperty("user.home") + File.separator + "Documents";
         path += File.separator + "TRANSACTION_REPORT";
         File customDir = new File(path);
@@ -57,29 +65,62 @@ public class ExcelGenerationServiceImpl implements ExcelGenerationService {
         try  {
             FileOutputStream outputStream = new FileOutputStream(excelFilePath);
             workbook.write(outputStream);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-
-//        try {
-//            MimeMessage message = mailSender.crea
-//            teMessage();
+//            MimeMessage message = mailSender.createMimeMessage();
 //            MimeMessageHelper helper = new MimeMessageHelper(message, true);
 //
-//            helper.setTo("ICT Application");
+//            helper.setTo("majonitrish522@gmail.com");
 //            helper.setSubject("Excel Report");
-//            helper.setText("Please find the attached Excel report.");
+//            helper.setText("Please find the attached Excel transaction report.");
 //
 //            helper.addAttachment("report.xlsx", new File(excelFilePath));
-//
+//            System.out.println(helper);
 //            mailSender.send(message);
-//        } catch (MessagingException e) {
-//            e.printStackTrace();
-//        } catch (MessagingException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
+            sendSimpleMessage();
+        } catch (IOException e) {
+            log.error("Error sending email with attachment: ", e);
+        }
+//        catch (MessagingException e){
+//      }
     }
-}
+
+    public void sendSimpleMessage(
+            ) {
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("majonitrish522@gmail.com");
+        message.setTo("trish.majoni@nbs.co.zw");
+        message.setSubject("Excel Report");
+        message.setText("Please find the attached Excel transaction report.");
+        mailSender.send(message);
+        System.out.println(message);
+
+    }
+
+    }
+
+
+
+
+//@Bean
+//public JavaMailSender getJavaMailSender() {
+//    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+//    mailSender.setHost("smtp.gmail.com");
+//    mailSender.setPort(587);
+//
+//    mailSender.setUsername("my.gmail@gmail.com");
+//    mailSender.setPassword("password");
+//
+//    Properties props = mailSender.getJavaMailProperties();
+//    props.put("mail.transport.protocol", "smtp");
+//    props.put("mail.smtp.auth", "true");
+//    props.put("mail.smtp.starttls.enable", "true");
+//    props.put("mail.debug", "true");
+//
+//    return mailSender;
+//============================================================
+//spring.mail.host=smtp.gmail.com
+//spring.mail.port=587
+//spring.mail.username=<login user to smtp server>
+//spring.mail.password=<login password to smtp server>
+//spring.mail.properties.mail.smtp.auth=true
+//spring.mail.properties.mail.smtp.starttls.enable=true
--- src/main/java/zw/co/nbs/main/BusinessImplConfig.java
+++ src/main/java/zw/co/nbs/main/BusinessImplConfig.java
@@ -3,10 +3,17 @@ package zw.co.nbs.main;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import zw.co.nbs.business.Impl.ExcelGenerationServiceImpl;
 import zw.co.nbs.business.api.ExcelGenerationService;
 import zw.co.nbs.connection.Impl.GatewayConnImpl;
 import zw.co.nbs.connection.api.GatewayConn;
+
+import javax.mail.internet.MimeMessage;
+import java.io.InputStream;
 
 @Configuration
 public class BusinessImplConfig {
@@ -18,6 +25,51 @@ public class BusinessImplConfig {
 
     @Bean
     public ExcelGenerationService excelGenerationService(final ApplicationContext context){
-        return new ExcelGenerationServiceImpl();
+        return new ExcelGenerationServiceImpl(context);
+    }
+
+    @Bean
+    public JavaMailSender mailSender(){
+        return new JavaMailSender() {
+            @Override
+            public MimeMessage createMimeMessage() {
+                return null;
+            }
+
+            @Override
+            public MimeMessage createMimeMessage(InputStream inputStream) throws MailException {
+                return null;
+            }
+
+            @Override
+            public void send(MimeMessage mimeMessage) throws MailException {
+
+            }
+
+            @Override
+            public void send(MimeMessage... mimeMessages) throws MailException {
+
+            }
+
+            @Override
+            public void send(MimeMessagePreparator mimeMessagePreparator) throws MailException {
+
+            }
+
+            @Override
+            public void send(MimeMessagePreparator... mimeMessagePreparators) throws MailException {
+
+            }
+
+            @Override
+            public void send(SimpleMailMessage simpleMailMessage) throws MailException {
+
+            }
+
+            @Override
+            public void send(SimpleMailMessage... simpleMailMessages) throws MailException {
+
+            }
+        };
     }
 }
